2b154917be9d8a8551a27c80ee5dec47180ab7cd
Sumaya Altamimi <smsomi9@hotmail.com>
2021-03-28 06:33:29 +0300

    started modify

--- nmapAutomated.sh
+++ nmapAutomated.sh
@@ -1,6 +1,26 @@
 #! /bin/bash
-target=${1}
+target='192.168.100.16'
 
+
+Echo ''
+Echo ''
+
+Echo ' ************************* Auto-Nmap Analyzer Started ************************* '
+Echo ''
+Echo ''
 {
-Nmap -sC -sV -p- TS -vv ${target}
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+Nmap ${target}
+
 }
+Echo ''
+Echo ''
+
+Echo ' ************************* Auto-Nmap Analyzer Done ************************* '
+Echo ''
+Echo ''
