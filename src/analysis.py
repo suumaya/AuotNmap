703c29043e5e632d1d622c5be4f631b7ec8c86b2
Sumaya Altamimi <smsomi9@hotmail.com>
2021-03-31 08:24:58 +0300

    good

--- src/analysis.py
+++ src/analysis.py
@@ -23,19 +23,26 @@ host = sys.argv[2]
 df = pd.read_csv(file_name)
 base_color = sb.color_palette()[0]
 
-#visuals
-#print(df.head())
-
-#Plt.figure()
+# services visuals
 sb.set_style("darkgrid")
+ax = sb.countplot(x="SERVICE", data=df, palette="Set3")
+ax.figure.savefig("service-result.png")
 
-#port visuals
-ax1 = sb.countplot(x="SERVICE", data=df, palette="Set3")
-ax1.figure.savefig("service-result.png")
+## ports visuals
+#port_counts = df['PORT'].value_counts()
+#plt.pie(port_counts, labels = port_counts.index, counterclock = False, startangle = 90, wedgeprops = {'width' : 0.4})
+#plt.axis('square');
+#plt.xticks(rotation=15)
+##enhance
+#plt.legend(loc = 6, bbox_to_anchor = (1.0, 0.5)) # legend to right of figure
+#plt.xticks(rotation = 15)
+#plt.title('Total Ports open right now')
+#plt.xlabel('Port')
+#plt.ylabel('Number of ports');
+#plt.savefig("port-result.png")
 
-#Port visuals
-ax1 = sb.countplot(x="PORT", data=df, palette="Set3")
-ax1.figure.savefig("port-result.png")
+
+
 
 #Report generation
 
@@ -43,52 +50,51 @@ logo_img = "logo.png"
 service_img = "service-result.png"
 port_img = "port-result.png"
 
-report_message1 ="Results of analyzing IP: " + host +" network computer\n"
-report_message2 = "\nServices are:\n"
-report_message3 = "Ports are:\n"
-report_message4 = "Cyber security team.\nSEC 504 project\nKing Saud University\n"
+report_message1 ="Results of analyzing IP: " + host +" network computer"
+report_message2 = "You have the following services open:"
+report_message3 = "You have the following ports open! try to close unnecessarily ports:"
 
-#convert to final_report.pdf
+report_message4 = "Thank You for using the service..."
+report_message5 = "Cyber security team."
+report_message6 = "King Saud University - SEC 504 project"
 
-# write in docx, kali not support ms but worked 
-#mydoc = docx.Document()
-#
-#mydoc.add_picture(logo_img)
-#mydoc.add_heading(report_message1, 0)
-#
-#mydoc.add_paragraph(report_message2)
-#mydoc.add_picture(service_img)
-#
-#mydoc.add_paragraph(report_message3)
-#mydoc.add_picture(port_img)
-#
-#mydoc.add_heading(report_message4, 1)
-#
-#mydoc.save("initial_report.docx")
+report_message7 = "Current features include:"
+report_message8 = "1. Collecting network data automatically"
+report_message9 = "2. Analyzing the collected network data"
+report_message10 = "3. Generate reports of the network status"
 
 
-# convert
-#convert("initial_report.docx", "final_report.pdf")
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font("Arial", size = 15)
+pdf.set_font('Arial','B',16);
+pdf.set_text_color(176,224,230)
 
 pdf.cell(200, 10, txt = report_message1,ln = 1, align = 'C')
-pdf.image(logo_img, w=pdf.w/2.0, h=pdf.h/4.0)
+pdf.image(logo_img, w=pdf.w/2.0, h=pdf.h/4.0,x=50)
+pdf.cell(200, 10, txt = report_message6,ln = 4, align = 'C')
+
+pdf.set_text_color(0,76,153)
+pdf.cell(200, 10, txt = report_message7,ln = 4, align = 'C')
+pdf.cell(200, 10, txt = report_message8,ln = 4, align = 'C')
+pdf.cell(200, 10, txt = report_message9,ln = 4, align = 'C')
+pdf.cell(200, 10, txt = report_message10,ln = 4, align = 'C')
+
 
 pdf.add_page()
+pdf.set_text_color(0,76,153)
 pdf.cell(200, 10, txt = report_message2,ln = 2, align = 'C')
-pdf.image(service_img, w=pdf.w/2.0, h=pdf.h/4.0)
+pdf.image(service_img, w=pdf.w/2.0, h=pdf.h/4.0,x=50)
 pdf.ln(0.15)
  
 pdf.cell(200, 10, txt = report_message3,ln = 3, align = 'C')
-pdf.image(port_img, w=pdf.w/2.0, h=pdf.h/4.0)
-pdf.ln(0.15)
+##pdf.image(port_img, 10,30,100)
+#pdf.image(port_img, w=pdf.w/2.0, h=pdf.h/4.0, x=50)
+#pdf.ln(0.15)
 
-pdf.add_page()
 pdf.cell(200, 10, txt = report_message4,ln = 4, align = 'C')
+pdf.cell(200, 10, txt = report_message5,ln = 4, align = 'C')
+
 
 pdf.output("final_report.pdf",'F')
 
-#images
 
